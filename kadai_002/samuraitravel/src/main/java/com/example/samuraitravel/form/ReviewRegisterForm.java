90bfa17c690a3292a4a2ccf12370a4ef021a9b06
suabru <sskm01ito27@gmail.com>
2025-05-15 02:58:26 +0900

    Update ReviewRegisterForm.java

--- kadai_002/samuraitravel/src/main/java/com/example/samuraitravel/form/ReviewRegisterForm.java
+++ kadai_002/samuraitravel/src/main/java/com/example/samuraitravel/form/ReviewRegisterForm.java
@@ -1,16 +1,21 @@
 package com.example.samuraitravel.form;
-import lombok.AllArgsConstructor;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
+
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class ReviewRegisterForm {
-	private Integer houseId;
-	
-	private Integer userId;
-	
-	private Integer evaluation;
-	
-	private String comment;
+public class ReviewForm {
+  @NotNull(message = "評価数を選択してください。")
+  @Range(min=1, max=5, message = "評価は１～５で選択してください。")
+  private Integer score;
+  
+  @NotBlank(message = "評価コメントを入力してください。")
+  @Length(max = 300, message = "評価コメントは300文字以内で入力してください。")
+  private String content;
+  
 }
