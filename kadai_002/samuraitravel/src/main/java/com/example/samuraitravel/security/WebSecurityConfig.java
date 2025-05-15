ec8322d8271db51e4b4dd01d190b3baf3d423251
suabru <sskm01ito27@gmail.com>
2025-05-15 10:45:10 +0900

    Update WebSecurityConfig.java

--- kadai_002/samuraitravel/src/main/java/com/example/samuraitravel/security/WebSecurityConfig.java
+++ kadai_002/samuraitravel/src/main/java/com/example/samuraitravel/security/WebSecurityConfig.java
@@ -13,12 +13,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class WebSecurityConfig {
-
-    @Bean
-     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	
+	@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests((requests) -> requests                
-            		.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**", "/houses", "/houses/{id}", "/stripe/webhook").permitAll()  // すべてのユーザーにアクセスを許可するURL            
+            	.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**", "/houses", "/houses/{id}", "/stripe/webhook", "/houses/{id}/reviews").permitAll()  // すべてのユーザーにアクセスを許可するURL
                 .requestMatchers("/admin/**").hasRole("ADMIN")  // 管理者にのみアクセスを許可するURL
                 .anyRequest().authenticated()                   // 上記以外のURLはログインが必要（会員または管理者のどちらでもOK）
             )
@@ -32,9 +32,9 @@ public class WebSecurityConfig {
             .logout((logout) -> logout
                 .logoutSuccessUrl("/?loggedOut")  // ログアウト時のリダイレクト先URL
                 .permitAll()
-            		)
-            .csrf().ignoringRequestMatchers("/stripe/webhook");           
-            
+                )
+            		
+            .csrf(csrf -> csrf.ignoringRequestMatchers("/stripe/webhook"));            
         return http.build();
     }
     
@@ -42,5 +42,5 @@ public class WebSecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
+
 }
