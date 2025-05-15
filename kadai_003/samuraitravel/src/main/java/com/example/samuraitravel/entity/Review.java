6c5751461670e89e9e246733411414df35a4994d
suabru <sskm01ito27@gmail.com>
2025-05-15 11:11:47 +0900

    Update Review.java

--- kadai_003/samuraitravel/src/main/java/com/example/samuraitravel/entity/Review.java
+++ kadai_003/samuraitravel/src/main/java/com/example/samuraitravel/entity/Review.java
@@ -10,34 +10,39 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
 import lombok.Data;
 
 @Entity
 @Table(name = "reviews")
 @Data
 public class Review {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
+	@Column(name ="id")
 	private Integer id;
 	
 	@ManyToOne
-	@JoinColumn(name = "house_id")
+	@JoinColumn (name = "house_id")
 	private House house;
 	
 	@ManyToOne
 	@JoinColumn(name = "user_id")
 	private User user;
 	
-    @Column(name = "score")
-    private Integer score;
-    
-    @Column(name = "content")
-    private String content;
-    
-    @Column(name = "created_at", insertable = false, updatable = false)
-    private Timestamp createdAt;
-    
-    @Column(name = "updated_at", insertable = false, updatable = false)
-    private Timestamp updatedAt;
+	@Column(name = "comment")
+	private String comment;
+
+	@Column(name = "evaluation ")
+	private Integer evaluation ;
+
+	
+	@Column(name = "created_at", insertable = false, updatable = false)
+	private Timestamp createdAt;
+	
+	@Column(name = "updated_at", insertable = false, updatable = false)
+	private Timestamp updatedAt;
+		
+	
 }
